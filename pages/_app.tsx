fa90a6ba561a524bda1d06d37cc8566e2db35122
Kevin Yu <kevinyu567@gmail.com>
2021-07-19 11:37:05 -0700

    Update app from tailwind template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import {AppProps} from 'next/app';
 import Layout from '../components/Layout';
 
 import '../scss/index.css';
+//import 'tailwindcss/tailwind.css'
 
 
 export default function App({ Component, pageProps }: AppProps) {
