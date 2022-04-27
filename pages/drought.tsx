16af60ca6e3df51ae71aeab7e39dd8754e83b848
Kevin Yu <kevinyu567@gmail.com>
2022-04-26 20:52:34 -0700

    Use luxon

--- pages/drought.tsx
+++ pages/drought.tsx
@@ -1,30 +1,20 @@
 import {useState, useEffect} from 'react';
 import Head from 'next/head';
-import moment from 'moment';
+import {DateTime} from 'luxon';
 
 
 export default function Drought() {
-    const [time, setTime] = useState(moment());
+    const [time, setTime] = useState(DateTime.now());
+    const lastCommitTimestamp = DateTime.fromISO('2021-02-06T21:11:19Z');
 
-    // Set interval on mount to update datetime every second
     useEffect(() => {
-        const timerID = setInterval(() => setTime(moment()), 1000);
-        return function cleanup() {
-            clearInterval(timerID);
-        }
+        const timerID = setInterval(() => setTime(DateTime.now()), 1000);
+        return () => clearInterval(timerID);
     }, [])
 
-    // TODO: clean this up
     const parseTime = () => {
-        const lastCommitTimestamp = '2021-02-06T21:11:19Z';
-        const parseSingular = (x: number) => String(x).length === 1 ? `0${x}` : x;
-
-        const seconds = time.diff(lastCommitTimestamp, 'seconds') % 60;
-        const minutes = time.diff(lastCommitTimestamp, 'minutes') % 60;
-        const hours = time.diff(lastCommitTimestamp, 'hours') % 24;
-        const days = time.diff(lastCommitTimestamp, 'days');
-
-        return `${days}:${parseSingular(hours)}:${parseSingular(minutes)}:${parseSingular(seconds)}`;
+        const diff = time.diff(lastCommitTimestamp, ['days', 'hours', 'minutes', 'seconds']);
+        return diff.toFormat('d:hh:mm:ss');
     }
 
     return (
