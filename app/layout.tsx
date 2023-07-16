55f3c5635af7673d332663487ff55bd101cf3586
Kevin Yu <kevinyu567@gmail.com>
2023-07-15 17:39:04 -0700

    Use inter, adjust margins

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import {Metadata} from 'next';
 import {ReactNode} from 'react';
+import {Inter} from 'next/font/google';
 
 import '../styles/index.scss';
 
@@ -11,13 +12,17 @@ export const metadata: Metadata = {
     }
 }
 
+const inter = Inter({
+    subsets: ['latin']
+});
+
 export default function Layout(props: {children: ReactNode}) {
     return (
         <html className="dark">
             <head>
                 <meta charSet="utf-8" />
             </head>
-            <body className="text-dark dark:text-white dark:bg-midnight">
+            <body className="text-dark dark:text-white dark:bg-midnight" style={inter.style}>
                 {props.children}
             </body>
         </html>
--- components/Section.tsx
+++ components/Section.tsx
@@ -13,7 +13,7 @@ export default function Section(props: {children: ReactNode, secondary?: boolean
 
 export function SectionHeading(props: {children: ReactNode, className?: string}) {
     return (
-        <h1 className={'text-4xl font-bold mb-6' + (props.className ? ` ${props.className}` : '')}>
+        <h1 className={'text-4xl font-bold mb-4' + (props.className ? ` ${props.className}` : '')}>
             {props.children}
         </h1>
     )
