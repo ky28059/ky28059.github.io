058319321cdcf318a033c8d2694c07fe68d9a719
Kevin Yu <kevinyu567@gmail.com>
2024-04-23 15:15:39 -0400

    Finish corpus obfuscator text

--- app/(scripts)/obfuscation/page.tsx
+++ app/(scripts)/obfuscation/page.tsx
@@ -4,19 +4,14 @@ import ObfuscationContent from './ObfuscationContent';
 
 
 export const metadata: Metadata = {
-    title: 'C Obfuscation',
-    description: 'Generator for C programs obfuscated from the given corpus.'
+    title: 'Corpus Obfuscation',
+    description: 'Generator for C programs obfuscated from a given text corpus.'
 }
 
 export default function CodeforcesHacks() {
     return (
         <>
             <SectionHeading>Corpus Obfuscator</SectionHeading>
-
-            <p className="mb-3">
-                ...
-            </p>
-
             <ObfuscationContent />
         </>
     )
