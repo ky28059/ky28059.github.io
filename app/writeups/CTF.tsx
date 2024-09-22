09f670d6f809993b4bd95a92990845d8bca08a6a
Kevin Yu <kevinyu567@gmail.com>
2024-09-21 22:25:13 -0400

    Update writeup styling + support for tags, images

--- app/writeups/CTF.tsx
+++ app/writeups/CTF.tsx
@@ -15,15 +15,15 @@ type CTFProps = {
 }
 export default function CTF(props: CTFProps) {
     return (
-        <Disclosure as="div" defaultOpen>
-            <DisclosureButton className="relative flex items-center rounded px-3 py-1 w-full mb-1 text-left text-primary dark:text-primary-dark hover:text-black dark:hover:text-white bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 transition duration-200">
+        <Disclosure as="div" defaultOpen className="-ml-3">
+            <DisclosureButton className="relative flex items-center rounded px-3 py-1 w-full mb-3 text-left text-primary dark:text-primary-dark hover:text-black dark:hover:text-white bg-black/5 dark:bg-white/5 hover:bg-black/10 dark:hover:bg-white/10 transition duration-200">
                 <PiFlagPennantFill className="absolute -left-6 text-grapefruit mt-0.5" />
                 <h3>{props.name}</h3>
 
                 <HiChevronDown className='ml-auto size-5 p-1 rounded-full bg-black/5 dark:bg-white/5 transition-transform duration-200 rotate-180 ui-open:rotate-0' />
             </DisclosureButton>
 
-            <DisclosurePanel as="ol" className="flex flex-col pl-6">
+            <DisclosurePanel as="ol" className="flex flex-col gap-1.5 pl-3">
                 {props.children}
             </DisclosurePanel>
         </Disclosure>
@@ -35,21 +35,57 @@ type ChallType = 'misc' | 'web' | 'crypto' | 'pwn' | 'rev' | 'osint' | 'apk' | '
 export type WriteupData = {
     href: string,
     type?: ChallType,
-    name: string
+    src?: string,
+    name: string,
+    tags?: string[]
 }
 export function Writeup(props: WriteupData) {
     return (
-        <li className="group relative font-mono">
-            <BiSubdirectoryRight className="absolute -left-7 text-xl inset-y-0 my-auto text-secondary dark:text-secondary-dark group-hover:text-primary dark:group-hover:text-primary-dark" />
-
-            <a href={props.href} target="_blank" rel="noopener noreferrer">
-                {props.name}
-            </a>
-            {props.type && (
-                <span className="ml-1.5 select-none px-1.5 py-0.5 text-xs rounded-full bg-grapefruit/30 text-grapefruit font-semibold hover:no-underline">
-                    {props.type}
-                </span>
+        <a
+            className="group flex gap-5 relative font-mono border rounded overflow-clip hover:no-underline text-primary dark:text-primary-dark hover:text-black dark:hover:text-white border-white/25 hover:border-white/50 transition duration-150"
+            href={props.href}
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            {props.src ? (
+                <img
+                    className="w-36 h-20 object-cover object-center flex-none bg-white"
+                    src={props.src}
+                    alt={props.name}
+                />
+            ) : (
+                <div className="w-36 h-20 bg-black/30 flex-none" />
             )}
-        </li>
+
+            <div className="py-3.5">
+                <p className="mb-0.5">
+                    {props.type && (
+                        <span className="text-secondary dark:text-secondary-dark">
+                            {props.type}
+                        </span>
+                    )}
+                    {props.type && (
+                        <span className="text-tertiary dark:text-tertiary-dark mx-0.5">/</span>
+                    )}
+                    {props.name}
+                </p>
+
+                <div className="flex gap-1">
+                    {props.type && (
+                        <span className="select-none px-1.5 py-0.5 text-xs rounded-full bg-grapefruit/30 text-grapefruit font-semibold">
+                            {props.type}
+                        </span>
+                    )}
+                    {props.tags && props.tags.map((s) => (
+                        <span
+                            className="select-none px-1.5 py-0.5 text-xs rounded-full bg-grapefruit/30 text-grapefruit font-semibold"
+                            key={s}
+                        >
+                            {s}
+                        </span>
+                    ))}
+                </div>
+            </div>
+        </a>
     )
 }
--- app/writeups/page.tsx
+++ app/writeups/page.tsx
@@ -60,7 +60,9 @@ const ctfs: CTFData[] = [
         name: 'jailCTF 2024',
         writeups: [{
             href: 'https://gist.github.com/ky28059/e9170fdb1df865a9811ef434c097e781',
-            name: 'jellyjail'
+            name: 'jellyjail',
+            // src: 'https://gist.github.com/user-attachments/assets/39b2cd25-132a-43bf-84a4-a7b29b78a38a'
+            src: 'https://gist.github.com/user-attachments/assets/c76f30bc-7abd-46fc-ae5b-bb5b697e2515'
         }]
     },
     {
@@ -68,7 +70,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             type: 'forensics',
             href: 'https://gist.github.com/ky28059/6c2437fd2e120bad3af82c8fcd36e5a5',
-            name: 'I like it RAW'
+            name: 'I like it RAW',
+            src: 'https://gist.github.com/user-attachments/assets/b33fa93a-d7bd-49b5-bbe7-98e4b7ecb1ab'
         }]
     },
     {
@@ -76,7 +79,9 @@ const ctfs: CTFData[] = [
         writeups: [{
             type: 'misc',
             href: 'https://gist.github.com/ky28059/467d31ff2f536b79f517da4f85229be4',
-            name: 'SKK?'
+            name: 'SKK?',
+            src: 'https://gist.github.com/user-attachments/assets/c2bc1f12-ce89-4805-bd88-a3902e539636',
+            tags: ['imgproc']
         }, {
             type: 'misc',
             href: 'https://gist.github.com/ky28059/c7e3e83bb501755bda31683f0d359578',
@@ -100,7 +105,9 @@ const ctfs: CTFData[] = [
         writeups: [{
             type: 'misc',
             href: 'https://gist.github.com/ky28059/2f279fff831a5be3d6b5fbb8a6d86057',
-            name: 'msfrogofwar3'
+            name: 'msfrogofwar3',
+            src: 'https://gist.github.com/user-attachments/assets/8a765e88-8838-49ae-bc11-91094fc83f97',
+            tags: ['stockfish']
         }]
     },
     {
@@ -108,19 +115,23 @@ const ctfs: CTFData[] = [
         writeups: [{
             type: 'osint',
             href: 'https://gist.github.com/ky28059/d6b13cd067d6b9d9fbc695080f9f7f7e',
-            name: 'Hip With the Youth'
+            name: 'Hip With the Youth',
+            src: 'https://gist.github.com/assets/60120929/28971e5d-31ec-4265-a0f0-8d05e3ca4a53'
         }, {
             type: 'osint',
             href: 'https://gist.github.com/ky28059/4e755cf1ba9a791899af3d4120c4a7d3',
-            name: 'An Unlikely Partnership'
+            name: 'An Unlikely Partnership',
+            src: 'https://gist.github.com/assets/60120929/a2e67ffd-5ced-4710-8a2b-bd42413ecedc'
         }, {
             type: 'osint',
             href: 'https://gist.github.com/ky28059/11ade1fe6eaaf1a864f60291a6ca3764',
-            name: 'The Weakest Link'
+            name: 'The Weakest Link',
+            src: 'https://gist.github.com/assets/60120929/d5c4010e-1a33-4414-80cf-d81e9a9145af'
         }, {
             type: 'osint',
             href: 'https://gist.github.com/ky28059/f55af843f32868e72f3fcb0bc9b732a2',
-            name: 'Night'
+            name: 'Night',
+            src: 'https://gist.github.com/assets/60120929/f2e780a0-8aa8-4e8f-bc5f-920d2123116a'
         }, {
             type: 'web',
             href: 'https://gist.github.com/ky28059/6f7ef0a073b142a9ed5d460a930a9d1c',
@@ -132,7 +143,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             type: 'web',
             href: 'https://gist.github.com/ky28059/0c4ecb3e46bccbd295151d6d30c110d4',
-            name: 'Letter to the editor'
+            name: 'Letter to the editor',
+            src: 'https://gist.github.com/assets/60120929/baae2b59-e268-4aaa-af1e-fcfb460a0537'
         }]
     },
     {
@@ -145,17 +157,20 @@ const ctfs: CTFData[] = [
         {
             name: "Chill Beats",
             type: "osint",
-            href: "https://gist.github.com/ky28059/9b5dc250b9b0ddb51e05b9e690a82432"
+            href: "https://gist.github.com/ky28059/9b5dc250b9b0ddb51e05b9e690a82432",
+            src: "https://gist.github.com/assets/60120929/27ef132d-9b5b-440c-8afc-d5d63869a800"
         },
         {
             name: "calculator",
             type: "web",
-            href: "https://gist.github.com/ky28059/e8fc98d5b0be999b1409cc80a9077c54"
+            href: "https://gist.github.com/ky28059/e8fc98d5b0be999b1409cc80a9077c54",
+            src: "https://gist.github.com/assets/60120929/95c7d910-8738-45a5-a338-a74ec3ad3eab"
         },
         {
             name: "Raccoon Run",
             type: "crypto",
-            href: "https://gist.github.com/ky28059/e9d0957313af0d38811e6e845dab7a41"
+            href: "https://gist.github.com/ky28059/e9d0957313af0d38811e6e845dab7a41",
+            tags: ['randcrack']
         }]
     },
     {
@@ -163,17 +178,21 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "Krusty Katering",
             type: "misc",
-            href: "https://gist.github.com/ky28059/fd0ed1fb606c13ba6d686bcf07e6809d"
+            href: "https://gist.github.com/ky28059/fd0ed1fb606c13ba6d686bcf07e6809d",
+            src: "https://gist.github.com/assets/60120929/58a78bb3-8459-4bb1-a0cd-68f1e4491451"
         },
         {
             name: "100 degrees",
             type: "misc",
-            href: "https://gist.github.com/ky28059/6c7d64b108118ddb29d4d919b6635f2f"
+            href: "https://gist.github.com/ky28059/6c7d64b108118ddb29d4d919b6635f2f",
+            src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Lagrange_polynomial.svg/2560px-Lagrange_polynomial.svg.png'
         },
         {
             name: "Stop the voices",
             type: "misc",
-            href: "https://gist.github.com/ky28059/e1d26ae6652bc6f4c25535555034ca4b"
+            href: "https://gist.github.com/ky28059/e1d26ae6652bc6f4c25535555034ca4b",
+            src: 'https://gist.github.com/assets/60120929/38a486f2-e626-4f74-8059-e9efcdac027a',
+            tags: ['imgproc']
         }]
     },
     {
@@ -181,7 +200,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "A Block and a Hard Place",
             type: "misc",
-            href: "https://gist.github.com/ky28059/c193b08a07a433be4fac60426c47e92c"
+            href: "https://gist.github.com/ky28059/c193b08a07a433be4fac60426c47e92c",
+            src: 'https://gist.github.com/assets/60120929/e9d7885e-0836-4e5e-958b-653ec5ae846d'
         }]
     },
     {
@@ -189,7 +209,9 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "gacha",
             type: "misc",
-            href: "https://gist.github.com/ky28059/11bd9429d863a3f0e1e9f86579cfdc28"
+            href: "https://gist.github.com/ky28059/11bd9429d863a3f0e1e9f86579cfdc28",
+            src: 'https://gist.github.com/assets/60120929/7b8dd2a2-0d32-4f4d-86a1-4bf6d90b0844',
+            tags: ['imgproc']
         }, {
             name: "jsfudge",
             type: "misc",
@@ -205,7 +227,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "Just Wierd Things",
             type: "web",
-            href: "https://gist.github.com/ky28059/80491a6f0250146511a945a0f8bb49c2"
+            href: "https://gist.github.com/ky28059/80491a6f0250146511a945a0f8bb49c2",
+            tags: ['ssti']
         }]
     },
     {
@@ -217,7 +240,8 @@ const ctfs: CTFData[] = [
         }, {
             name: "Cheater",
             type: "osint",
-            href: "https://gist.github.com/ky28059/68631ca4de137e7fd855ab08b75531be"
+            href: "https://gist.github.com/ky28059/68631ca4de137e7fd855ab08b75531be",
+            src: 'https://gist.github.com/assets/60120929/a9c4a805-d97a-49d5-90db-c41ab3c3d68c'
         }]
     },
     {
@@ -225,7 +249,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "zshfuck",
             type: "misc",
-            href: "https://gist.github.com/ky28059/0510cb86a449925d38dc9a0c6a312b35"
+            href: "https://gist.github.com/ky28059/0510cb86a449925d38dc9a0c6a312b35",
+            src: 'https://gist.github.com/assets/60120929/db124457-8412-460a-bc46-85ba7ccd0f4c'
         }]
     },
     {
@@ -240,19 +265,28 @@ const ctfs: CTFData[] = [
         name: "iCTF 2023",
         writeups: [{
             name: "escape_from_markov",
-            href: "https://gist.github.com/ky28059/e63c6d26f19301f2024b6231677b4bbd"
+            href: "https://gist.github.com/ky28059/e63c6d26f19301f2024b6231677b4bbd",
+            src: 'https://gist.github.com/assets/60120929/8cf8fab8-aaee-4d26-a65e-1eac9beb6834'
         }, {
             name: "IslandParty",
-            href: "https://gist.github.com/ky28059/e7fed465703f2943e482cd2d6618ceb4"
+            href: "https://gist.github.com/ky28059/e7fed465703f2943e482cd2d6618ceb4",
+            src: 'https://gist.github.com/assets/60120929/498dea08-696d-4f31-ad8c-387738aec7f4',
+            tags: ['imgproc']
         }, {
             name: "CI Ninja",
-            href: "https://gist.github.com/ky28059/86cd4a8f5da44c495fd8109a82c94626"
+            href: "https://gist.github.com/ky28059/86cd4a8f5da44c495fd8109a82c94626",
+            src: 'https://gist.github.com/assets/60120929/3bfa6bd3-1e80-45f5-8a63-0dffb448d422',
+            tags: ['prompt engineering']
         }, {
             name: "Mystical Castle",
-            href: "https://gist.github.com/ky28059/a6dae8a017d440c08ad7514c0312557d"
+            href: "https://gist.github.com/ky28059/a6dae8a017d440c08ad7514c0312557d",
+            src: 'https://gist.github.com/assets/60120929/5bbb480a-7174-45fe-b0c9-261e521f8e5a',
+            tags: ['pddl']
         }, {
             name: "CI Ninja 2",
-            href: "https://gist.github.com/ky28059/968252b99245abcc823ca1a77e610ef5"
+            href: "https://gist.github.com/ky28059/968252b99245abcc823ca1a77e610ef5",
+            src: 'https://gist.github.com/assets/60120929/d48ccd62-af7a-4b1a-a92b-1b4f0552f165',
+            tags: ['prompt engineering']
         }, {
             name: "Stop the model thief!",
             href: "https://gist.github.com/ky28059/2f74ca229358e1d7ef508cfa76379d35"
@@ -263,7 +297,8 @@ const ctfs: CTFData[] = [
         writeups: [{
             name: "typescrip",
             type: "misc",
-            href: "https://gist.github.com/ky28059/a851fdabc90d887a61af81c071f6f0ce"
+            href: "https://gist.github.com/ky28059/a851fdabc90d887a61af81c071f6f0ce",
+            src: 'https://user-images.githubusercontent.com/60120929/271787201-15c1a765-7c83-4eb0-b2de-f48a37f15e57.png'
         }]
     }
 ]
