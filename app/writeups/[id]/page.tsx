ab15b3c6368afbff55b0de2988c1e1f7e4059725
Kevin Yu <kevinyu567@gmail.com>
2025-05-12 15:23:52 -0700

    Parse `h3`s as link headings

--- app/writeups/[id]/page.tsx
+++ app/writeups/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { ctfs } from '../ctfs';
 import Markdown from 'react-markdown';
+
+// Components
 import CopyCodeBlock from '../../../components/CopyCodeBlock';
+import LinkHeading from '../../../components/LinkHeading';
 
 
 export default async function WriteupPage({ params }: { params: Promise<{ id: string }> }) {
@@ -9,7 +12,7 @@ export default async function WriteupPage({ params }: { params: Promise<{ id: st
 
     return (
         <div>
-            <main className="max-w-5xl mx-auto text-sm [&_h1]:text-5xl [&_h1]:font-semibold [&_h1]:mb-8 [&_blockquote]:text-secondary [&_blockquote]:space-y-3 [&_blockquote]:border-l-4 [&_blockquote]:border-secondary [&_blockquote]:pl-5 [&_blockquote]:mb-5 [&>_p]:my-3 [&_img]:my-5 [&_ul]:list-disc [&_ul]:pl-6 [&>p>code]:bg-black/20 [&>p>code]:text-primary [&>p>code]:px-2 [&>p>code]:py-1 [&>p>code]:rounded [&_img]:rounded">
+            <main className="max-w-5xl mx-auto text-sm [&_h1]:text-5xl [&_h1]:font-semibold [&_h1]:mb-8 [&_blockquote]:text-secondary [&_blockquote]:space-y-3 [&_blockquote]:border-l-4 [&_blockquote]:border-secondary [&_blockquote]:pl-5 [&_blockquote]:mb-5 [&>_p]:my-3 [&_img]:my-5 [&_ul]:list-disc [&_ul]:pl-6 [&>p>code]:bg-black/20 [&>p>code]:text-primary [&>p>code]:px-2 [&>p>code]:py-1 [&>p>code]:rounded [&_img]:rounded [&_li]:my-2">
                 <Markdown
                     components={{
                         pre(props) {
@@ -32,6 +35,16 @@ export default async function WriteupPage({ params }: { params: Promise<{ id: st
                                     {children}
                                 </pre>
                             );
+                        },
+                        h3(props) {
+                            const { children, className } = props;
+                            const id = String(children).toLowerCase().replaceAll(' ', '-');
+
+                            return (
+                                <LinkHeading id={id} className={'text-3xl font-bold mt-10 mb-4 ' + (className || '')}>
+                                    {children}
+                                </LinkHeading>
+                            )
                         }
                     }}
                 >
