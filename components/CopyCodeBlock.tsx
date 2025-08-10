a417cd0dfa5b4cd7503a392cf7a794d446e99def
Kevin Yu <kevinyu567@gmail.com>
2025-08-10 14:05:56 -0700

    Add language icons to code blocks

--- components/CopyCodeBlock.tsx
+++ components/CopyCodeBlock.tsx
@@ -2,6 +2,21 @@
 
 import SyntaxHighlighter from '@/components/SyntaxHighlighter';
 
+// Icons
+import {
+    SiGnubash,
+    SiGo,
+    SiHtml5,
+    SiJavascript,
+    SiKotlin,
+    SiPerl,
+    SiPython,
+    SiTypescript,
+    SiYaml
+} from 'react-icons/si';
+import { TbJson } from 'react-icons/tb';
+import { MdDifference } from 'react-icons/md';
+
 
 type ScriptOutputProps = {
     className?: string,
@@ -9,12 +24,22 @@ type ScriptOutputProps = {
     language?: string
 }
 export default function CopyCodeBlock(props: ScriptOutputProps) {
+    const Icon = languageToIcon(props.language);
+
     return (
         <div className={'border border-white/10 rounded-lg' + (props.className ? ` ${props.className}` : '')}>
-            <p className="text-xs rounded-t-lg px-4 py-0.5 text-secondary">
-                Code{props.language && ` (${props.language})`}:{' '}
+            <p className="flex items-center text-xs rounded-t-lg px-3 py-0.5 text-secondary">
+                {Icon ? (
+                    <><Icon className="text-sm mr-1.5" /> {props.language}</>
+                ) : (
+                    <>Code{props.language && ` (${props.language})`}</>
+                )}
+
                 {/* TODO: animate this with a popup or something */}
-                <button onClick={() => navigator.clipboard.writeText(props.children)} className="hover:underline">
+                <button
+                    onClick={() => navigator.clipboard.writeText(props.children)}
+                    className="ml-auto hover:underline"
+                >
                     (copy)
                 </button>
             </p>
@@ -25,4 +50,49 @@ export default function CopyCodeBlock(props: ScriptOutputProps) {
             </div>
         </div>
     )
+}
+
+function languageToIcon(language?: string) {
+    if (!language) return null;
+
+    switch (language) {
+        case 'js':
+        case 'javascript':
+            return SiJavascript;
+
+        case 'ts':
+        case 'typescript':
+            return SiTypescript;
+
+        case 'py':
+        case 'python':
+            return SiPython;
+
+        case 'kt':
+        case 'kotlin':
+            return SiKotlin;
+
+        case 'html':
+            return SiHtml5;
+
+        case 'perl':
+            return SiPerl;
+
+        case 'json': return TbJson;
+
+        case 'yml':
+        case 'yaml':
+            return SiYaml;
+
+        case 'sh':
+        case 'bash':
+            return SiGnubash;
+
+        case 'diff':
+            return MdDifference;
+
+        case 'go': return SiGo;
+    }
+
+    return null;
 }
