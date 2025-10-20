16e803c411c6f924ea7e40618ba6e6ff0b5374b1
Kevin Yu <kevinyu567@gmail.com>
2025-10-20 17:05:57 -0400

    Update "second-year student", redesign date tags

--- app/(landing)/LandingHeader.tsx
+++ app/(landing)/LandingHeader.tsx
@@ -1,4 +1,7 @@
-import ThemeToggle from '../../components/ThemeToggle';
+import type { ReactNode } from 'react';
+
+// Components
+import ThemeToggle from '@/components/ThemeToggle';
 import GithubContributionChart from './GithubContributionChart';
 
 // Icons
@@ -25,7 +28,7 @@ export default function LandingHeader() {
                     </h1>
 
                     <p className="mb-3 max-w-3xl text-primary">
-                        Gunn High School graduate and second-year student studying Computer Science @ Purdue
+                        Gunn High School graduate and third-year student studying Computer Science @ Purdue
                         University. Interested in art, photography, web development, and robotics.
                     </p>
                     <p className="mb-3.5 text-primary">
@@ -47,34 +50,42 @@ export default function LandingHeader() {
                     </div>
                 </div>
 
-                <div className="hidden lg:block">
+                <div className="hidden lg:block text-pretty ml-6 flex-none">
                     <p className="text-tertiary text-sm">
                         You may know me from:
                     </p>
                     <ul className="text-secondary mb-4">
-                        <li>President @ b01lers (2025-p.)</li>
-                        <li>Webmaster @ b01lers (2024-25)</li>
+                        <li>President @ b01lers <AffiliationDate>2025-p.</AffiliationDate></li>
+                        <li>Webmaster @ b01lers <AffiliationDate>2024-25</AffiliationDate></li>
                     </ul>
                     <ul className="text-secondary">
-                        <li>Controls, webmaster @ GRT (2021-23)</li>
-                        <li>VP @ United Computations (2021-23)</li>
-                        <li>Lead Org. @ GunnHacks (2020-23)</li>
-                        <li>WATT (2020-23)</li>
+                        <li>Controls, webmaster @ GRT <AffiliationDate>2021-23</AffiliationDate></li>
+                        <li>VP @ United Computations <AffiliationDate>2021-23</AffiliationDate></li>
+                        <li>Lead Org. @ GunnHacks <AffiliationDate>2020-23</AffiliationDate></li>
+                        <li>WATT <AffiliationDate>2020-23</AffiliationDate></li>
                     </ul>
 
                     <p className="text-tertiary text-sm mt-6">
                         Other affiliations:
                     </p>
                     <ul className="text-secondary">
-                        <li>Contractor @ CAIS (2025)</li>
-                        <li>UTA @ Purdue CS (2025)</li>
-                        <li>TA @ MIT BWSI (2024)</li>
-                        <li>Intern @ Stanford GLAM (2023)</li>
+                        <li>Contractor @ CAIS <AffiliationDate>2025</AffiliationDate></li>
+                        <li>UTA @ Purdue CS <AffiliationDate>2025</AffiliationDate></li>
+                        <li>TA @ MIT BWSI <AffiliationDate>2024</AffiliationDate></li>
+                        <li>Intern @ Stanford GLAM <AffiliationDate>2023</AffiliationDate></li>
                     </ul>
                 </div>
             </div>
 
             <GithubContributionChart />
         </header>
+    )
+}
+
+function AffiliationDate(props: { children: ReactNode }) {
+    return (
+        <span className="text-xs rounded-sm bg-white/10 px-1 py-0.5 text-primary">
+            {props.children}
+        </span>
     )
 }
